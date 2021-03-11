e758d00b6cdeba9dda9564afdc351f009fdb533b
Gambit <gambitdeclined@protonmail.com>
2021-03-11 12:45:16 -0500

    Remove maintenance page
    Removing the maintenace page until we have a heartbeat endpoint

--- components/infrastructure/Navigation.vue
+++ components/infrastructure/Navigation.vue
@@ -1316,7 +1316,7 @@ export default {
             })
         .catch((err)=>{
             console.log(err)
-            this.$router.push('/maintenance')
+            // this.$router.push('/maintenance')
         })
     } else {
         this.tutorialFlixSteps.pop()
